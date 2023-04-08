4afc2536100c86786afe013a8d4c9f12b272e829
Joynana5 <joynaew@gmail.com>
2023-04-08 16:36:51 -0400

    working-ish

--- your_space/src/App.jsx
+++ your_space/src/App.jsx
@@ -1,46 +1,52 @@
-
 import React, { createContext , useEffect, useState} from "react";
 import SignIn from "./components/SignIn/SignIn.jsx";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Posts from "./components/Posts/Posts";
 import CreateProfile from "./components/Profile/CreateProfile";
-import { Routes, Route } from "react-router-dom";
-
-
-
-
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 const AuthenticationContext = createContext()
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const local = localStorage.getItem('TOKEN')
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
   const handleSignIn = () => {
+    const local = localStorage.getItem('TOKEN')
     if (local) {
       setIsAuthenticated(true)
     }
+    else {
+      setIsAuthenticated(false)
+    }
   }
 
-useEffect(() => {
-    handleSignIn()
-  })
-  return (
-    <AuthenticationContext.Provider value={isAuthenticated}>{isAuthenticated ? (
-      <>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Profile />} />
-          <Route path="/create-profile" exact element={<CreateProfile />} />
-          <Route path="/posts" exact element={<Posts />} />
-        </Routes>
-      </>
-    ) : (
-      <SignIn onSignIn={handleSignIn} />
-    )}
-    
-    </AuthenticationContext.Provider>
+  const handleLogout = () => {
+    localStorage.removeItem('TOKEN');
+    setIsAuthenticated(false);
+    navigate('/signin');
+  }
 
+  useEffect(() => {
+    handleSignIn()
+  }, [])
+
+  return (
+    <AuthenticationContext.Provider value={isAuthenticated}>
+      {isAuthenticated ? (
+        <>
+          <Header onLogout={handleLogout} />
+          <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/create-profile" element={<CreateProfile />} />
+            <Route path="/posts" element={<Posts />} />
+          </Routes>
+        </>
+      ) : (
+        <SignIn onSignIn={handleSignIn} />
+      )}
+    </AuthenticationContext.Provider>
   );
 }
 
