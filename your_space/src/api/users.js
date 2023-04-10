4388d4541e3dc9c78553106ef05b340561c0d9b8
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-04-10 13:43:06 -0400

    trying to get the route to go to profile homepage

--- your_space/src/api/users.js
+++ your_space/src/api/users.js
@@ -1,41 +1,39 @@
 import api from "./apiConfig";
 
-const LOCALSTORAGE_KEY = 'TOKEN';
-const LOCALSTORAGE_ID = 'USER_ID';
-const LOCALSTORAGE_USER_NAME = 'USER_NAME';
-
-
+const LOCALSTORAGE_KEY = "TOKEN";
+const LOCALSTORAGE_ID = "USER_ID";
+const LOCALSTORAGE_USER_NAME = "USER_NAME";
 
 export function getUser() {
   const token = localStorage.getItem(LOCALSTORAGE_KEY);
   return token;
 }
 
-
 export async function signIn(username, password) {
-  const response = await api.post('/api/login/', {username, password})
-  
+  const response = await api.post("/api/login/", { username, password });
+
   if (response.data.token) {
     localStorage.setItem(LOCALSTORAGE_KEY, response.data.token);
-    
+
     if (response.data.id) {
       localStorage.setItem(LOCALSTORAGE_ID, response.data.id);
       console.log(response.data.id);
     }
-  
+
     if (response.data.username) {
       localStorage.setItem(LOCALSTORAGE_USER_NAME, response.data.username);
     }
 
     return response.data;
-
-  } else if(response.data.isUser) {
-    return {login: false, message: "User does not exist, please sign up or try again"}
+  } else if (response.data.isUser) {
+    return {
+      login: false,
+      message: "User does not exist, please sign up or try again",
+    };
   } else if (response.data.isPassword) {
-    return {login: false, message: "Password is incorrect, please try again"}
+    return { login: false, message: "Password is incorrect, please try again" };
   }
 }
-
 
 export function isAuthenticated() {
   const token = localStorage.getItem(LOCALSTORAGE_KEY);
@@ -44,27 +42,26 @@ export function isAuthenticated() {
   const user = {
     token,
     userId,
-    userName
-  }
+    userName,
+  };
   return user !== null;
 }
 
 export async function getProfile(id, body) {
-  const response = await api.patch(`/profiles/${id}`, body)
-  return response.data
+  const response = await api.patch(`/profiles/${id}`, body);
+  return response.data;
 }
 
 export async function signUp(username, password, email) {
-  const response = await api.post('/create-user/', {
-    method: 'post',
+  const response = await api.post("/create-user/", {
+    method: "post",
 
     headers: {
-    'Content-Type': 'application/json'
-  },
+      "Content-Type": "application/json",
+    },
     username,
     email,
-    password
-  })
-  return response.data  
-
-} 
+    password,
+  });
+  return response.data;
+}
