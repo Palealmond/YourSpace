51597cab33835888dd00c484a299a248abd73b70
Joynana5 <joynaew@gmail.com>
2023-04-10 19:19:54 -0400

    useNavigate for posts working

--- your_space/src/components/Posts/Posts.jsx
+++ your_space/src/components/Posts/Posts.jsx
@@ -1,31 +1,47 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../../api/apiConfig';
 
 export default function Posts() {
-  const [formData, setFormData] = useState({ 
+  const [formData, setFormData] = useState({
     title: '',
     content: '',
     subject: '',
     category: '',
   });
-  
-  const [posts, setPosts] = useState([]);
 
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await api.post('/posts/', { ...formData});
+  //     const data = await response.json();
+  //     setPosts([...posts, data]);
+  //     setFormData({ subject: '', category: '', content: '', title: '' }); //
+  //     navigate('/')
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/posts', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await response.json();
-      setPosts([...posts, data]);
+      const response = await api.post('/posts/', { ...formData, title: 'haghaha' });
+      console.log(response);;
+      setPosts([...posts, response.data]);
       setFormData({ subject: '', category: '', content: '' });
+      navigate('/') // navigate to user's profile page
     } catch (error) {
       console.log(error);
     }
+  };
+  
+  
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value });
   };
 
   return (
@@ -34,6 +50,21 @@ export default function Posts() {
         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div className="p-6 bg-white border-b border-gray-200">
             <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+                <label className="text-xl text-gray-600">
+                  Title
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="title"
+                    id="title"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    value={formData.title}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
               <div className="mb-4">
                 <label className="text-xl text-gray-600">
                   Subject
@@ -44,50 +75,47 @@ export default function Posts() {
                   className="border-2 border-gray-300 p-2 w-full"
                   name="subject"
                   id="subject"
-                  placeholder="Title"
+                  placeholder="Subject"
                   value={formData.subject}
-                  onChange={(e) =>
-                    setFormData({ ...formData, subject: e.target.value })
-                  }
+                  onChange={handleChange}
                 />
               </div>
-              <div className="flex p-1">
-                <label className="text-xl text-gray-600 gap-4">
+              <div className="mb-4">
+                <label className="text-xl text-gray-600">
                   Category
                 </label>
+                <br />
                 <select
-                  className="ml-8 border-2 border-gray-300 border-r p-2"
+                  className="border-2 border-gray-300 p-2 w-full"
                   name="category"
                   value={formData.category}
-                  onChange={(e) =>
-                    setFormData({ ...formData, category: e.target.value })
-                  }
+                  onChange={handleChange}
                 >
-                  <option>Select</option>
-                  <option>Art & Photography</option>
-                  <option>Automotive</option>
-                  <option>Blogging</option>
-                  <option>Books and Stories</option>
-                  <option>Dreams and the Supernatural</option>
-                  <option>Fashion, Style, Shopping</option>
-                  <option>Food and Restaurants</option>
-                  <option>Friends</option>
-                  <option>Games</option>
-                  <option>Goals, Plans, Hopes</option>
-                  <option>Jobs, Work, Careers</option>
-                  <option>Life</option>
-                  <option>Music</option>
-                  <option>Parties and Nightlife</option>
-                  <option>Pets and Animals</option>
-                  <option>Podcast</option>
-                  <option>Quiz/Survey</option>
-                  <option>Romance and Relationship</option>
-                  <option>School, College, University</option>
-                  <option>Sports</option>
-                  <option>Travel and Places</option>
-                  <option>Web, HTML, Tech</option>
-                  <option>Writing and Poetry</option>
-                  <option>Your Space</option>
+                <option value="">Select</option>
+                <option>Art & Photography</option>
+                <option>Automotive</option>
+                <option>Blogging</option>
+                <option>Books and Stories</option>
+                <option>Dreams and the Supernatural</option>
+                <option>Fashion, Style, Shopping</option>
+                <option>Food and Restaurants</option>
+                <option>Friends</option>
+                <option>Games</option>
+                <option>Goals, Plans, Hopes</option>
+                <option>Jobs, Work, Careers</option>
+                <option>Life</option>
+                <option>Music</option>
+                <option>Parties and Nightlife</option>
+                <option>Pets and Animals</option>
+                <option>Podcast</option>
+                <option>Quiz/Survey</option>
+                <option>Romance and Relationship</option>
+                <option>School, College, University</option>
+                <option>Sports</option>
+                <option>Travel and Places</option>
+                <option>Web, HTML, Tech</option>
+                <option>Writing and Poetry</option>
+                <option>Your Space</option> 
                 </select>
               </div>
               <div className="mb-8">
@@ -100,9 +128,7 @@ export default function Posts() {
                   className="border-2 border-gray-500 w-full my-4"
                   required
                   value={formData.content}
-                  onChange={(e) =>
-                    setFormData({ ...formData, content: e.target.value })
-                  }
+                  onChange={handleChange}
                 ></textarea>
                 <button
                   type="submit"
@@ -118,6 +144,9 @@ export default function Posts() {
     </>
   );
 }
+
+
+
 
 
 
